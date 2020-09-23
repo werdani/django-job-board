8aa6bb993878e3bc23d18f62830a5a98b337af3c
werdani <https://github.com/werdani/django-job-board.git>
2020-09-23 13:30:32 +0200

    add paginator

--- job/views.py
+++ job/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from .models import job
+from django.core.paginator import Paginator
 
 # Create your views here.
 
 def job_list(request):
     job_list=job.objects.all()
-    context= {'jobs' : job_list}
+    paginator=Paginator(job_list,1)
+    page_number=request.GET.get('page')
+    page_obj=paginator.get_page(page_number)
+    context= {'jobs' : page_obj}
     return render(request,'job/job_list.html',context)
     
 def job_detail(request,id):
