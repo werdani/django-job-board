8026c29d50eee76f00668df3df7f6c3b6500fc24
werdani <https://github.com/werdani/django-job-board.git>
2020-09-24 08:10:17 +0200

    add slug in url

--- job/models.py
+++ job/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
 JOB_TYPE = (
@@ -19,6 +20,11 @@ class job(models.Model):
     experience=models.IntegerField(default=1)
     category=models.ForeignKey('Category',on_delete=models.CASCADE)
     image=models.ImageField(upload_to=image_upload)
+    slug=models.SlugField(blank=True, null=True)
+
+    def save(self,*args,**kwargs):
+        self.slug=slugify(self.title)
+        super(job,self).save(*args,**kwargs)
 
     def __str__(self):
         return self.title
--- job/views.py
+++ job/views.py
@@ -12,8 +12,8 @@ def job_list(request):
     context= {'jobs' : page_obj}
     return render(request,'job/job_list.html',context)
     
-def job_detail(request,id):
-    job_detail=job.objects.get(id=id)
+def job_detail(request,slug):
+    job_detail=job.objects.get(slug=slug)
     context={'job':job_detail}
     return render(request,'job/job_detail.html',context)
     
