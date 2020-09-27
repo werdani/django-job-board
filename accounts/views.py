2744f9b6b75ffb88ba77f91d981c13f419e5e45c
werdani <https://github.com/werdani/django-job-board.git>
2020-09-27 16:16:15 +0200

    add signup

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .forms import SignupForm
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
+
+def signup(request):
+    if request.method=="POST":
+        form = SignupForm(request.POST)
+        if form.is_valid:
+            form.save()
+            username=form.cleaned_data['username']
+            password=form.cleaned_data['password1']
+            user=authenticate(username=user, passwoed=password)
+            login(request,user)
+            return redirect('/accounts/profile')
+    else:
+        form = SignupForm()
+    return render(request,'registration/signup.html',{'form':form}) 
