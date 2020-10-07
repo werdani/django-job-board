f3057447dbb0848669d69cdb1b54b6688cd7d665
werdani <https://github.com/werdani/django-job-board.git>
2020-10-07 22:32:44 +0200

    create class based views

--- job/api.py
+++ job/api.py
@@ -2,10 +2,31 @@ from .models import job
 from .serializers import Jobserializers
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import generics
 
 @api_view(['GET'])
-def joblistapi(request):
+def job_list_api(request):
     all_jobs = job.objects.all()
     data =Jobserializers(all_jobs,many=True).data
 
     return Response({'data':data})
+
+
+
+@api_view(['GET'])
+def job_details_api(request,id):
+    job_detail=job.objects.get(id=id)
+    data =Jobserializers(job_detail).data
+
+    return Response({'data':data})
+
+class JobListApi(generics.ListAPIView):
+    queryset = job.objects.all()
+    serializer_class=Jobserializers
+
+
+class JobDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class=Jobserializers
+    queryset = job.objects.all()
+    lookup_field='id'
+
--- job/urls.py
+++ job/urls.py
@@ -8,6 +8,14 @@ urlpatterns = [
     path('add',views.add_job , name='add_job'),
     path('<str:slug>',views.job_detail , name='job_detail'),
     #api
-    path('api/list',api.joblistapi , name='joblistapi'),
+    path('api/jobs',api.job_list_api , name='job_list_api'),
+    path('api/jobs/<int:id>',api.job_details_api , name='job_details_api'),
+
+    #class based viwes
+    path('api/v2/jobs',api.JobListApi.as_view() , name='JobListApi'),
+    path('api/v2/jobs/<int:id>',api.JobDetail.as_view() , name='job_details_api'),
+
+
+
     
 ]
